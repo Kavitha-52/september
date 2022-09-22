94673910e29b7ddb1cd2f95177bbe8b6dfa00e71
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-09-22 19:56:44 +0530

    delete element in array

---
+++ bitwise/strings/delete_element.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int arr[]={1,2,3,4,5,6},n,i,pos;
+	n=sizeof(arr)/sizeof(arr[0]);
+	printf("enter position:\n");
+	scanf("%d",&pos);
+	if(pos >=n+1)
+		printf("deletion not possible:\n");
+	else
+	{
+		for(i=pos-1;i<n;i++)
+		{
+			arr[i]=arr[i+1];
+		}
+		printf("resultant array is :");
+		for(i=0;i<n-1;i++)
+		{
+			printf("%d ",arr[i]);
+		}
+	}
+}
