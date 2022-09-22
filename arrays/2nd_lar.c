3cf544c0fa7554889ea0eb1cbb3a3d0394c677ed
Kavitha-52 <98574222+Kavitha-52@users.noreply.github.com>
2022-09-22 20:06:40 +0530

    2nd largest_element in array

---
+++ arrays/2nd_lar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+        int arr[]={12,34,4,123,87,99},i,n;
+  n=sizeof(arr)/sizeof(arr[0]);
+        int large=arr[0],sl=0;
+        for(i=1;i<n;i++)
+        {
+                if(arr[i]>large)
+                {
+                        sl=large;
+                        large=arr[i];
+                }
+                else if(arr[i]>sl)
+                        sl=arr[i];
+        }
+        printf("largest element:%d\n second large element:%d\n",large,sl);
+}
+
